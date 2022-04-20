7dc8ff918c18bf3404e689509b0bc1221bd345f8
Emre Cal <emrecaal@gmail.com>
2022-04-21 02:11:32 +0300

    .

--- src/containers/SignIn/index.js
+++ src/containers/SignIn/index.js
@@ -1,13 +1,22 @@
 import Header from '../../components/Header';
 
-function SignIn(){
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+
+function SignIn() {
     return (
         <div>
             <div>
                 <Header/>
             </div>
-            <div>
-                sign in
+            <div className="container mt-5">
+                <div className="row align-items-center justify-content-center">
+                    <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+                        <AccountCircle sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+                        <TextField label="With sx" variant="standard" />
+                    </Box>
+                </div>
             </div>
         </div>
     )
