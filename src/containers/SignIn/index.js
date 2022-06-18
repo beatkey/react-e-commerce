f7dfadcffe757c6911f20ea80982f0d218e474e9
Beatkey <emrecaal@gmail.com>
2022-06-18 23:45:18 +0300

    login form

--- src/containers/SignIn/index.js
+++ src/containers/SignIn/index.js
@@ -1,22 +1,70 @@
+import {useState} from 'react';
+import {useNavigate} from 'react-router-dom';
+
 import Header from '../../components/Header';
 
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import AccountCircle from '@mui/icons-material/AccountCircle';
+import Button from '@mui/material/Button';
+import {Alert} from '@mui/material';
 
 function SignIn() {
+    let navigate = useNavigate();
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const [errorAlert, setErrorAlert] = useState(false);
+    const [successAlert, setSuccessAlert] = useState(false);
+
+    function Submit(e){
+        e.preventDefault();
+
+        setErrorAlert(false)
+        setSuccessAlert(false)
+
+        if(username=="emre" && password=="123"){
+            sessionStorage.setItem("logged-in", true)
+            setSuccessAlert(true)
+            navigate("/")
+        }else{
+            setErrorAlert(true)
+            sessionStorage.setItem("logged-in", false)
+        }
+    }
+
     return (
         <div>
             <div>
                 <Header/>
             </div>
             <div className="container mt-5">
-                <div className="row align-items-center justify-content-center">
-                    <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-                        <AccountCircle sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-                        <TextField label="With sx" variant="standard" />
+                <form onSubmit={Submit} className="row align-items-center justify-content-center">
+                    {errorAlert && <Alert severity="error">Username or password is wrong!</Alert>}
+                    {successAlert && <Alert severity="success">Login</Alert>}
+                    <Box className="mt-3" sx={{ display: 'flex', alignItems: 'flex-end' }}>
+                        <TextField
+                            required={true}
+                            autoComplete="username"
+                            onChange={e => setUsername(e.target.value)}
+                            label="Username"
+                            variant="standard"
+                        />
                     </Box>
-                </div>
+                    <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+                        <TextField
+                            required={true}
+                            autoComplete="current-password"
+                            type="password"
+                            onChange={e => setPassword(e.target.value)}
+                            label="Password"
+                            variant="standard"
+                        />
+                    </Box>
+                    <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+                        <Button type="submit" className="mt-4" variant="contained">Send</Button>
+                    </Box>
+                </form>
             </div>
         </div>
     )
