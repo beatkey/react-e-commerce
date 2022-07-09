ca0b73ad2daac7812c352fda897efd1617d75d23
Beatkey <emrecaal@gmail.com>
2022-07-09 17:13:02 +0300

    header dropdown menu

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -8,14 +8,25 @@ import {LoggedInContext} from '../../context/loggedIn';
 import Button from '@mui/material/Button';
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 import {TableRows, ViewComfy, ViewStream, ViewWeek} from '@mui/icons-material';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
 
 
 export default function Header(props) {
+    let location = useLocation();
     const {view, ChangeView} = props;
     const [cart] = useContext(BasketContext);
     const [loggedIn, setLoggedIn] = useContext(LoggedInContext);
     const [basketDrawer, setBasketDrawer] = useState(false);
-    let location = useLocation();
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
 
     const basketDrawerToggle = (open) => (event) => {
         if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
@@ -46,6 +57,8 @@ export default function Header(props) {
         }
     };
 
+    console.log(loggedIn)
+
     return (
         <header className="container-fluid">
             <div className="row align-items-center">
@@ -61,6 +74,30 @@ export default function Header(props) {
                                 About
                             </Button>
                         </Link>
+                        <div>
+                            <Button
+                                id="basic-button"
+                                aria-controls={open ? 'basic-menu' : undefined}
+                                aria-haspopup="true"
+                                aria-expanded={open ? 'true' : undefined}
+                                onClick={handleClick}
+                            >
+                                Dashboard
+                            </Button>
+                            <Menu
+                                id="basic-menu"
+                                anchorEl={anchorEl}
+                                open={open}
+                                onClose={handleClose}
+                                MenuListProps={{
+                                    'aria-labelledby': 'basic-button',
+                                }}
+                            >
+                                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                                <MenuItem onClick={handleClose}>My account</MenuItem>
+                                <MenuItem onClick={handleClose}>Logout</MenuItem>
+                            </Menu>
+                        </div>
                         {location.pathname=="/" &&
                         <Button onClick={() => ChangeView()} variant="contained" color="success" endIcon={<ViewIcon />}>
                             View
